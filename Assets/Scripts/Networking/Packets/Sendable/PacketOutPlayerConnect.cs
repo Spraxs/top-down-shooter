89c19d2a758864395060e3f64682658a15e75240
Spraxs <jaimy.putter@outlook.com>
2019-11-02 13:11:25 +0100

    Updated connect packet

--- Assets/Scripts/Networking/Packets/Sendable/PacketOutPlayerConnect.cs
+++ Assets/Scripts/Networking/Packets/Sendable/PacketOutPlayerConnect.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,5 +10,19 @@ public class PacketOutPlayerConnect : SendablePacket
         WriteShort(0);
 
         WriteInt(5);
+
+        WriteDouble(1.0D);
+
+        WriteDouble(2.5D);
+
+        WriteLong(CurrentTimeMillis());
+    }
+
+    private static readonly DateTime Jan1st1970 = new DateTime
+    (1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+    public static long CurrentTimeMillis()
+    {
+        return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
     }
 }

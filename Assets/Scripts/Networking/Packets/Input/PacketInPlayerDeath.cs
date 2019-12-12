b43d76529604d7c1d3e1c9b8436dbfb695121c51
Spraxs <jaimy.putter@outlook.com>
2019-12-12 12:41:28 +0100

    Added Death and Respawn packets

---
+++ Assets/Scripts/Networking/Packets/Input/PacketInPlayerDeath.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using UnityEngine;
+
+[PacketId(8)]
+public class PacketInPlayerDeath : PacketIn
+{
+    public PacketInPlayerDeath() : base(true)
+    {
+    }
+
+    public override void HandleData(MemoryStream ms)
+    {
+        long playerId = ReadLong(ms);
+
+        float deathPositionX = (float) ReadDouble(ms);
+        float deathPositionY = (float)ReadDouble(ms);
+
+        Vector2 deathPosition = new Vector2(deathPositionX, deathPositionY);
+    }
+}
---
+++ Assets/Scripts/Networking/Packets/Input/PacketInPlayerRespawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[PacketId(9)]
+public class PacketInPlayerRespawn : PacketIn
+{
+    public PacketInPlayerRespawn() : base(true)
+    {
+    }
+
+    public override void HandleData(MemoryStream ms)
+    {
+        long playerId = ReadLong(ms);
+
+        float respawnPositionX = (float) ReadDouble(ms);
+        float respawnPositionY = (float) ReadDouble(ms);
+
+        Vector2 respawnPosition = new Vector2(respawnPositionX, respawnPositionY);
+    }
+}
